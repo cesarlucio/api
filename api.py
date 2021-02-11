f29c006702e77dc43a5fa6eb6f03e6e5f3ac5110
cesarlucio <cesar.silva@ze.delivery>
2021-02-11 14:43:17 -0300

    status code retorna 201 quando cadastra pessoa

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from banco import Banco
-
+import flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,10 +14,14 @@ def cadastrar():
     name = dados['name']
     document = dados['document']
     age = dados['age']
+
     banco = Banco()
     resposta = banco.cadastrar(dados)
 
-    return resposta
+    if resposta:
+        return flask.Response(status=201)
+
+    return str(resposta)
 
 @app.route("/pessoa/buscar", methods=['GET'])
 def buscar():
@@ -29,8 +33,8 @@ def buscar():
     else:
         resposta = banco.buscar("")
 
-
     return str(resposta)
+
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
 
